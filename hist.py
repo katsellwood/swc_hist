ecac453a9aa4824ca80c806769fbe380b23ec2e5
Kat Sellwood <kathryn.sellwood@noaa.gov>
2020-02-28 10:40:08 -0500

    maybe final script

--- hist.py
+++ hist.py
@@ -1,6 +1,7 @@
 # Randomizing data, generating statistics and histogram
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 mu = 80
 sigma =  10
@@ -10,3 +11,7 @@ print("Random Normal Array Mean Centered",x[:10])
 
 print("mean", np.mean(x))
 
+plt.hist(x)
+plt.show()
+
+print("I am beyond awesome")
